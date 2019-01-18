1e2d79d785473c6c112103e40b4e18faf6e4d4d7
jangseokgyu <43026098+jangseokgyu@users.noreply.github.com>
2019-01-19 00:38:05 +0900

    Update and rename bluetooth rc to bluetooth RC.c

--- bluetooth RC.c
+++ bluetooth RC.c
@@ -28,40 +28,40 @@ void loop() {
       digitalWrite(IN2, LOW);
       digitalWrite(IN3, HIGH);
       digitalWrite(IN4, LOW);
-      analogWrite(ENA,200);
-      analogWrite(ENB,200);
+      //analogWrite(ENA,200);
+      //analogWrite(ENB,200);
       
     } else if (cmd == 'l') {    
       digitalWrite(IN1, LOW);
       digitalWrite(IN2, LOW);
       digitalWrite(IN3, HIGH);
       digitalWrite(IN4, LOW);
-      analogWrite(ENA,200);
-      analogWrite(ENB,200);
+      //analogWrite(ENA,200);
+      //analogWrite(ENB,200);
       
     } else if (cmd == 'r') {     
       digitalWrite(IN1, HIGH);
       digitalWrite(IN2, LOW);
       digitalWrite(IN3, LOW);
       digitalWrite(IN4, LOW);
-      analogWrite(ENA,200);
-      analogWrite(ENB,200);
+      //analogWrite(ENA,200);
+      //analogWrite(ENB,200);
       
     } else if (cmd ==  'b') {  
       digitalWrite(IN1, LOW);
       digitalWrite(IN2, HIGH);
       digitalWrite(IN3, LOW);
       digitalWrite(IN4, HIGH);
-      analogWrite(ENA,200);
-      analogWrite(ENB,200);
+      //analogWrite(ENA,200);
+      //analogWrite(ENB,200);
       
     } else if (cmd == 's') {    
       digitalWrite(IN1, LOW);
       digitalWrite(IN2, LOW);
       digitalWrite(IN3, LOW);
       digitalWrite(IN4, LOW);
-      analogWrite(ENA,0);
-      analogWrite(ENB,0);
+      //analogWrite(ENA,0);
+      //analogWrite(ENB,0);
     }
   }
 }
