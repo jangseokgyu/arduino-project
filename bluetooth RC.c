a32b69661c525e635e0ea87d94b56a7feb2c59ed
jangseokgyu <43026098+jangseokgyu@users.noreply.github.com>
2019-01-19 13:07:23 +0900

    Update bluetooth RC.c

--- bluetooth RC.c
+++ bluetooth RC.c
@@ -1,13 +1,12 @@
 #include <SoftwareSerial.h>    
 SoftwareSerial BTSerial(7, 6);
 
-#define IN1 8
-#define IN2 9
-#define IN3 10
-#define IN4 11
-#define ENA 3
-#define ENB 5
-
+#define INa 8
+#define INb 9
+#define INc 10
+#define INd 11
+//#define ENA 6
+//#define ENB 5
 void setup() {
   BTSerial.begin(9600);    
   Serial.begin(9600);      
@@ -24,42 +23,42 @@ void loop() {
     Serial.println(cmd); 
 
     if ( cmd == 'f') {       
-      digitalWrite(IN1, HIGH);
-      digitalWrite(IN2, LOW);
-      digitalWrite(IN3, HIGH);
-      digitalWrite(IN4, LOW);
+      digitalWrite(INa, HIGH);
+      digitalWrite(INb, LOW);
+      digitalWrite(INc, HIGH);
+      digitalWrite(INd, LOW);
       //analogWrite(ENA,200);
       //analogWrite(ENB,200);
       
     } else if (cmd == 'l') {    
-      digitalWrite(IN1, LOW);
-      digitalWrite(IN2, LOW);
-      digitalWrite(IN3, HIGH);
-      digitalWrite(IN4, LOW);
+      digitalWrite(INa, LOW);
+      digitalWrite(INb, LOW);
+      digitalWrite(INc, HIGH);
+      digitalWrite(INd, LOW);
       //analogWrite(ENA,200);
       //analogWrite(ENB,200);
       
     } else if (cmd == 'r') {     
-      digitalWrite(IN1, HIGH);
-      digitalWrite(IN2, LOW);
-      digitalWrite(IN3, LOW);
-      digitalWrite(IN4, LOW);
+      digitalWrite(INa, HIGH);
+      digitalWrite(INb, LOW);
+      digitalWrite(INc, LOW);
+      digitalWrite(INd, LOW);
       //analogWrite(ENA,200);
       //analogWrite(ENB,200);
       
     } else if (cmd ==  'b') {  
-      digitalWrite(IN1, LOW);
-      digitalWrite(IN2, HIGH);
-      digitalWrite(IN3, LOW);
-      digitalWrite(IN4, HIGH);
+      digitalWrite(INa, LOW);
+      digitalWrite(INb, HIGH);
+      digitalWrite(INc, LOW);
+      digitalWrite(INd, HIGH);
       //analogWrite(ENA,200);
       //analogWrite(ENB,200);
       
     } else if (cmd == 's') {    
-      digitalWrite(IN1, LOW);
-      digitalWrite(IN2, LOW);
-      digitalWrite(IN3, LOW);
-      digitalWrite(IN4, LOW);
+      digitalWrite(INa, LOW);
+      digitalWrite(INb, LOW);
+      digitalWrite(INc, LOW);
+      digitalWrite(INd, LOW);
       //analogWrite(ENA,0);
       //analogWrite(ENB,0);
     }
